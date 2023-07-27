c71ba94b170ccabcf42dd075f99ab6ae01cf760f
Giovanni Braconi <thumpback2002.it@gmail.com>
2023-07-27 21:20:33 +0200

    Implemented kappa spawn

--- scripts/Main.gd
+++ scripts/Main.gd
@@ -1,8 +1,17 @@
 extends Node
-
- 
+var enemy 
+var enemyInstance
+var enemyPathCurve
 
 func _ready():
+	enemy=preload("res://scenes/Enemy.tscn")
+	enemyPathCurve= $EnemyPath.get_curve()
+	$EnemyTimer.start()
+	$EnemyTimer.connect("timeout",self,"_on_enemy_timer_timeout")
 	$Music.play()
-#func _process(delta):
-#	pass
+func _on_enemy_timer_timeout():
+	enemyInstance= enemy.instance()
+	enemyInstance.get_node("Sprite").scale=Vector2(7.88,7.88)
+	enemyInstance.position= enemyPathCurve.get_point_position(randi() % enemyPathCurve.get_point_count())
+	add_child(enemyInstance)
+	
