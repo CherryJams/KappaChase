45d89fe1b3e70b48cc40c1f2071cea7c31115331
Giovanni Braconi <thumpback2002.it@gmail.com>
2023-08-21 18:04:35 +0200

    added ball spawn sync, but the collision between the ball and the enemy works occasionally

--- scripts/Projectile.gd
+++ scripts/Projectile.gd
@@ -9,6 +9,9 @@ func _on_Timer_timeout():
 	position.y -= moveAmount
 	$Timer.start(GlobalTimer.beat/4)
 func _on_Projectile_area_entered(area):
-	if area.name == "Enemy":
-		area.queue_free()
-		queue_free()	
+    if area.name == "Enemy":
+        print("Enemy hit!")
+        area.queue_free()
+        queue_free()
+    else:
+        print("Non-enemy area entered:", area.name)
